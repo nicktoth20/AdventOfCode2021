05225c43fd59e875835d8e38427da50ad08b6878
Nick Toth <nick.toth@integrate.com>
2021-12-07 08:49:48 -0600

    Day 7

---
+++ AdventOfCode2021.Tests/Day7_should_ .cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    class Day7_should_
+    {
+        [Test]
+        public void Test1()
+        {
+            var day7 = new Day7();
+            var result = day7.ExecutePart1("Input/day07Example.txt");
+            Assert.AreEqual(37, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var day7 = new Day7();
+            var result = day7.ExecutePart1("Input/day07Input.txt");
+            Assert.AreEqual(37, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var day7 = new Day7();
+            var result = day7.ExecutePart2("Input/day07Example.txt");
+            Assert.AreEqual(168, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var day7 = new Day7();
+            var result = day7.ExecutePart2("Input/day07Input.txt");
+            Assert.AreEqual(37, result);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day7.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021
+{
+    public class Day7
+    {
+        private Parser _parser;
+
+        public Day7()
+        {
+            _parser = new Parser();
+        }
+
+        public int ExecutePart1(string fileName)
+        {
+            var lines = _parser.ParseLines(fileName).ToList();
+
+            var numbers = lines.First().Split(',').Select(int.Parse).ToList();
+
+            var min = numbers.Min();
+
+            var notFound = true;
+            var previous = int.MaxValue;
+            do
+            {
+                var totalForPosition = GetResultForPosition(numbers, min);
+                if (totalForPosition < previous)
+                {
+                    previous = totalForPosition;
+                }
+                else
+                {
+                    notFound = false;
+                }
+                min++;
+            } while (notFound);
+
+            return previous;
+        }
+
+        private int GetResultForPosition(IList<int> numbers, int position)
+        {
+            return numbers.Sum(number => Math.Abs(position - number));
+        }
+
+        public int ExecutePart2(string fileName)
+        {
+            var lines = _parser.ParseLines(fileName).ToList();
+
+            var numbers = lines.First().Split(',').Select(int.Parse).ToList();
+
+            var min = numbers.Min();
+
+            var notFound = true;
+            var previous = int.MaxValue;
+            do
+            {
+                var totalForPosition = GetResultForPosition2(numbers, min);
+                if (totalForPosition < previous)
+                {
+                    previous = totalForPosition;
+                }
+                else
+                {
+                    notFound = false;
+                }
+                min++;
+            } while (notFound);
+
+            return previous;
+        }
+
+        private int GetResultForPosition2(IList<int> numbers, int position)
+        {
+            return numbers.Sum(number => GetResultForNumberInPart2(Math.Abs(position - number)));
+        }
+
+        private int GetResultForNumberInPart2(int difference)
+        {
+            var count = 0;
+            for (var i = difference; i > 0; i--)
+            {
+                count += i;
+            }
+
+            return count;
+        }
+
+        public int Testing(int number)
+        {
+            var count = 0;
+            for (var i = number; i > 0; i--)
+            {
+                count += i;
+            }
+
+            return count;
+        }
+    }
+}
