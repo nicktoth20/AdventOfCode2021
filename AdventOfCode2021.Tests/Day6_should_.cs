99af8bb837ebdb771cf37bb6455cb8f6a8b7612a
Nick Toth <nick.toth@integrate.com>
2021-12-06 22:08:54 -0600

    Day 6

---
+++ AdventOfCode2021.Tests/Day6_should_.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    class Day6_should_
+    {
+        [Test]
+        public void Test1()
+        {
+            var day6 = new Day6();
+            var result = day6.ExecutePart1("Input/day06Example.txt", 18);
+            Assert.AreEqual(26, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var day6 = new Day6();
+            var result = day6.ExecutePart1("Input/day06Example.txt", 80);
+            Assert.AreEqual(5934, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var day6 = new Day6();
+            var result = day6.ExecutePart1("Input/day06Input.txt", 80);
+            Assert.AreEqual(351188, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var day6 = new Day6();
+            var result = day6.ExecutePart1("Input/day06Example.txt", 256);
+            Assert.AreEqual(26984457539, result);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            var day6 = new Day6();
+            var result = day6.ExecutePart1("Input/day06Input.txt", 256);
+            Assert.AreEqual(1595779846729, result);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day6.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021
+{
+    public class Day6
+    {
+        private Parser _parser { get; set; }
+        private Dictionary<int, long> _resultSet { get; set; }
+        private Dictionary<string, long> _kidSet { get; set; }
+
+        public Day6()
+        {
+            _parser = new Parser();
+            _resultSet = new Dictionary<int, long>();
+            _kidSet = new Dictionary<string, long>();
+        }
+
+        public long ExecutePart1(string fileName, int days)
+        {
+            var lines = _parser.ParseLines(fileName);
+            var initialFishes = Array.ConvertAll(lines.First().Split(','), int.Parse);
+            
+            var uniqueFish = initialFishes.Distinct();
+            var listOfFish = uniqueFish.Select(fish => new LanternFish(0, fish)).ToList();
+
+            //var listOfFish = initialFishes.Select(fish => new LanternFish(0, fish)).ToList();
+            foreach (var fish in listOfFish)
+            {
+                _resultSet[fish.Index] = GetKids(fish, days) + 1;
+            }
+
+            long totalFish = 0;
+
+            foreach (var fish in initialFishes)
+            {
+
+                totalFish += _resultSet[fish];
+            }
+
+            return totalFish;
+        }
+
+        private long GetKids(LanternFish fish, int days)
+        {
+            long total = 0;
+
+            var kids = fish.CreateKids(days);
+            total += kids.Count;
+            foreach (var kid in kids)
+            {
+                if (_kidSet.ContainsKey($"{kid.Index}-{kid.BirthDate}"))
+                {
+                    total += _kidSet[$"{kid.Index}-{kid.BirthDate}"];
+                }
+                else
+                {
+                    var k = GetKids(kid, days);
+                    _kidSet[$"{kid.Index}-{kid.BirthDate}"] = k;
+                    total += k;
+                }
+            }
+
+            return total;
+        }
+    }
+
+    public class LanternFish
+    {
+        public int BirthDate { get; set; }
+        public int Index { get; set; }
+
+        public LanternFish(int birthDate, int index)
+        {
+            BirthDate = birthDate;
+            Index = index;
+        }
+
+        public List<LanternFish> CreateKids(int days)
+        {
+            var fish = new List<LanternFish>();
+            var totalKids = TotalKids(days);
+
+            for (var i = 0; i < totalKids; i++)
+            {
+                var birthDate = (Index + 1) + (i * 7) + BirthDate;
+                if (birthDate <= days)
+                {
+                    fish.Add(new LanternFish(birthDate, 8));
+                }
+            }
+
+            return fish;
+        }
+
+        public int TotalKids(int days)
+        {
+            if (Index != 8)
+            {
+                return GetKidsForInitialFish(days);
+            }
+
+            return GetKidsForNewFish(days);
+        }
+
+        private int GetKidsForInitialFish(int days)
+        {
+            var start = days - Index;
+            return start / 7 + 1;
+        }
+
+        private int GetKidsForNewFish(int days)
+        {
+            var newDays = days - (BirthDate + Index);
+            return GetKidsForInitialFish(newDays) + 1;
+        }
+    }
+}
