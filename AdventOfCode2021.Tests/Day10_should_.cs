7a1c9d8985e342f4c739ac1655b434fce22f28ba
Nick Toth <nick.toth@integrate.com>
2021-12-09 23:32:50 -0600

    Day 10

---
+++ AdventOfCode2021.Tests/Day10_should_.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    class Day10_should_
+    {
+        [Test]
+        public void Test1()
+        {
+            var day10 = new Day10();
+            var result = day10.ExecutePart1("Input/day10Example.txt");
+            Assert.AreEqual(26397, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var day10 = new Day10();
+            var result = day10.ExecutePart1("Input/day10Input.txt");
+            Assert.AreEqual(415953, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var day10 = new Day10();
+            var result = day10.ExecutePart1("Input/day10Example.txt");
+            Assert.AreEqual(288957, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var day10 = new Day10();
+            var result = day10.ExecutePart1("Input/day10Input.txt");
+            Assert.AreEqual(2292863731, result);
+        }
+
+        // 162764112 not right
+    }
+}
---
+++ AdventOfCode2021/Day10.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode2021
+{
+    public class Day10
+    {
+        private Parser _parser;
+
+        public Day10()
+        {
+            _parser = new Parser();
+        }
+
+        public long ExecutePart1(string fileName)
+        {
+            var lines = _parser.ParseLines(fileName);
+            var score = new List<long>();
+
+            foreach (var line in lines)
+            {
+                long s = 0;
+                var stack = new Stack<char>();
+                var exit = false;
+                foreach (var character in line)
+                {
+                    if (exit)
+                    {
+                        continue;
+                    }
+                    switch (character)
+                    {
+                        case '(':
+                        case '[':
+                        case '{':
+                        case '<':
+                            stack.Push(character);
+                            break;
+                        case ')':
+                        case ']':
+                        case '}':
+                        case '>':
+                            var popped = stack.Pop();
+                            if (!Matches(popped, character))
+                            {
+                                //score += GetScore(character);
+                                exit = true;
+                            }
+                            break;
+                    }
+                }
+
+                if (!exit)
+                {
+                    while (stack.Count > 0)
+                    {
+                        s *= 5;
+                        s += GetMatchedScore(stack.Pop());
+                    }
+                    score.Add(s);
+                }
+            }
+
+            score.Sort();
+            
+            return score[score.Count / 2];
+        }
+
+        private bool Matches(char popped, char closingCharacter)
+        {
+            switch (closingCharacter)
+            {
+                case ')':
+                    return popped == '(';
+                case ']':
+                    return popped == '[';
+                case '}':
+                    return popped == '{';
+                default:
+                    return popped == '<';
+            }
+        }
+
+        private int GetScore(char closingCharacter)
+        {
+            switch (closingCharacter)
+            {
+                case ')':
+                    return 3;
+                case ']':
+                    return 57;
+                case '}':
+                    return 1197;
+                default:
+                    return 25137;
+            }
+        }
+
+        private int GetMatchedScore(char closingCharacter)
+        {
+            switch (closingCharacter)
+            {
+                case '(':
+                    return 1;
+                case '[':
+                    return 2;
+                case '{':
+                    return 3;
+                default:
+                    return 4;
+            }
+        }
+    }
+}
