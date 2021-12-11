07ee715cb1e2c87aa866a923c91b0420ed4e7668
Nick Toth <nick.toth@integrate.com>
2021-12-11 10:43:25 -0600

    Day 11

---
+++ AdventOfCode2021.Tests/Day11_should_.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    class Day11_should_
+    {
+        [Test]
+        public void Test1()
+        {
+            var day11 = new Day11();
+            var result = day11.ExecutePart1("Input/day11Example.txt");
+            Assert.AreEqual(1656, result);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var day11 = new Day11();
+            var result = day11.ExecutePart1("Input/day11Input.txt");
+            Assert.AreEqual(1656, result);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var day11 = new Day11();
+            var result = day11.ExecutePart2("Input/day11Example.txt");
+            Assert.AreEqual(195, result);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            var day11 = new Day11();
+            var result = day11.ExecutePart2("Input/day11Input.txt");
+            Assert.AreEqual(195, result);
+        }
+    }
+}
---
+++ AdventOfCode2021/Day11.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AdventOfCode2021
+{
+    public class Day11
+    {
+        private Parser _parser;
+        private Dictionary<string, Octopus> _octopi;
+        private int _flashed = 0;
+
+        public Day11()
+        {
+            _parser = new Parser();
+            _octopi = new Dictionary<string, Octopus>();
+        }
+
+        public int ExecutePart1(string fileName)
+        {
+
+            var lines = _parser.ParseLines(fileName).ToList();
+
+            for (var i = 0; i < lines.Count(); i++)
+            {
+                for (var j = 0; j < lines[i].Length; j++)
+                {
+                    _octopi[$"{i}-{j}"] = new Octopus(lines[i][j] - '0');
+                }
+            }
+
+            for (var i = 0; i < 100; i++)
+            {
+                IncrementOctopi();
+                while (_octopi.Values.Any(v => v.ShouldFlash()))
+                {
+                    FlashOctopi();
+                }
+            }
+
+            return _flashed;
+        }
+
+        public int ExecutePart2(string fileName)
+        {
+
+            var lines = _parser.ParseLines(fileName).ToList();
+
+            for (var i = 0; i < lines.Count(); i++)
+            {
+                for (var j = 0; j < lines[i].Length; j++)
+                {
+                    _octopi[$"{i}-{j}"] = new Octopus(lines[i][j] - '0');
+                }
+            }
+
+            var step = 1;
+            do
+            {
+                IncrementOctopi();
+                while (_octopi.Values.Any(v => v.ShouldFlash()))
+                {
+                    FlashOctopi();
+                }
+
+                if (_octopi.Values.All(o => o.Value == 0))
+                {
+                    return step;
+                }
+
+                step++;
+            } while (true);
+        }
+
+        public void IncrementOctopi()
+        {
+            foreach (var octopus in _octopi)
+            {
+                octopus.Value.Increment();
+            }
+        }
+
+        public void FlashOctopi()
+        {
+            var flashes = _octopi.Where(o => o.Value.ShouldFlash());
+            foreach (var octopus in flashes)
+            {
+                octopus.Value.Flash();
+                _flashed++;
+
+                var x = ParseX(octopus.Key);
+                var y = ParseY(octopus.Key);
+
+                // Top left
+                if (_octopi.ContainsKey($"{x - 1}-{y - 1}")) _octopi[$"{x - 1}-{y - 1}"].FlashIncrement();
+
+                // Top
+                if (_octopi.ContainsKey($"{x - 1}-{y}")) _octopi[$"{x - 1}-{y}"].FlashIncrement();
+
+                // Top right
+                if (_octopi.ContainsKey($"{x - 1}-{y + 1}")) _octopi[$"{x - 1}-{y + 1}"].FlashIncrement();
+
+                // left
+                if (_octopi.ContainsKey($"{x}-{y - 1}")) _octopi[$"{x}-{y - 1}"].FlashIncrement();
+
+                // right
+                if (_octopi.ContainsKey($"{x}-{y + 1}")) _octopi[$"{x}-{y + 1}"].FlashIncrement();
+
+                // bottom left
+                if (_octopi.ContainsKey($"{x + 1}-{y - 1}")) _octopi[$"{x + 1}-{y - 1}"].FlashIncrement();
+
+                // bottom
+                if (_octopi.ContainsKey($"{x + 1}-{y}")) _octopi[$"{x + 1}-{y}"].FlashIncrement();
+
+                // bottom right
+                if (_octopi.ContainsKey($"{x + 1}-{y + 1}")) _octopi[$"{x + 1}-{y + 1}"].FlashIncrement();
+
+            }
+        }
+
+        public int ParseX(string location)
+        {
+            return int.Parse(location.Split('-')[0]);
+        }
+
+        public int ParseY(string location)
+        {
+            return int.Parse(location.Split('-')[1]);
+        }
+    }
+
+    public class Octopus
+    {
+        public int Value { get; set; }
+
+        public Octopus(int value)
+        {
+            Value = value;
+        }
+
+        public void Increment()
+        {
+            Value++;
+        }
+
+        public bool ShouldFlash()
+        {
+            return Value > 9;
+        }
+
+        public void Flash()
+        {
+            Value = 0;
+        }
+
+        public void FlashIncrement()
+        {
+            if (Value > 0)
+            {
+                Increment();
+            }
+        }
+    }
+}
