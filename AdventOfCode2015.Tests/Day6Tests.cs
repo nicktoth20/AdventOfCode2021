25fc0a004779afde0b7586a3f325a524aacab1ac
Nick Toth <nick.toth@integrate.com>
2023-11-08 09:00:26 -0600

    Day 6 2015 part 2

--- AdventOfCode2015.Tests/Day6Tests.cs
+++ AdventOfCode2015.Tests/Day6Tests.cs
@@ -26,4 +26,21 @@ public class Day6Tests
         var result = day6.ExecutePart1("Input/day06Input.txt");
         result.Should().Be(543903); 
     }
+    
+    [TestCase("turn on 0,0 through 0,0", 1)]
+    [TestCase("turn off 0,0 through 0,0", 0)]
+    [TestCase("toggle 0,0 through 999,999", 2000000)]
+    public void Test3(string input, long expected)
+    {
+        var day6 = new Day6(new SingleLineInputParser());
+        day6.ExecutePart2(input).Should().Be(expected);
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var day6 = new Day6();
+        var result = day6.ExecutePart2("Input/day06Input.txt");
+        result.Should().Be(14687245); 
+    }
 }
--- AdventOfCode2015/Day6.cs
+++ AdventOfCode2015/Day6.cs
@@ -80,32 +80,61 @@ public class Day6
     public long ExecutePart2(string filePath)
     {
         var lines = _parser.ParseLines(filePath);
-        var niceStrings = 0;
+        var lights = new int[1000,1000];
         foreach (var line in lines) {
-            if (HasPair(line) && HasPalidrone(line)) {
-                niceStrings++;
+            if (line.StartsWith("turn on")) {
+                TurnOn(line, lights);
+            }
+            if (line.StartsWith("toggle")) {
+                Toggle(line, lights);
+            }
+            if (line.StartsWith("turn off")) {
+                TurnOff(line, lights);
             }
         }
-        return niceStrings;
+        return CountBrightness(lights);
     }
 
-    private bool HasPair(string input) {
-        for(var i = 0; i < input.Length - 1; i++) {
-            var pair = $"{input[i]}{input[i+1]}";
-            var index = input.LastIndexOf(pair);
-            if (index > i + 1) {
-                return true;
+    private static void TurnOn(string input, int[,] lights) {
+        var parts = input.Split(' ');
+        var first = parts[2].Split(',').Select(x => int.Parse(x)).ToList();
+        var second = parts[4].Split(',').Select(x => int.Parse(x)).ToList();
+        for(var i = first[0]; i <= second[0]; i++) {
+            for(var x = first[1]; x <= second[1]; x++) {
+                lights[i, x]++;
             }
         }
-        return false;
     }
 
-    private static bool HasPalidrone(string input) {
-        for(var i = 0; i < input.Length - 2; i++) {
-            if (input[i] == input[i + 2]) {
-                return true;
+    private static void TurnOff(string input, int[,] lights) {
+        var parts = input.Split(' ');
+        var first = parts[2].Split(',').Select(x => int.Parse(x)).ToList();
+        var second = parts[4].Split(',').Select(x => int.Parse(x)).ToList();
+        for(var i = first[0]; i <= second[0]; i++) {
+            for(var x = first[1]; x <= second[1]; x++) {
+                if (lights[i, x] > 0) lights[i, x]--;
             }
         }
-        return false;
+    }
+
+    private static void Toggle(string input, int[,] lights) {
+        var parts = input.Split(' ');
+        var first = parts[1].Split(',').Select(int.Parse).ToList();
+        var second = parts[3].Split(',').Select(int.Parse).ToList();
+        for(var i = first[0]; i <= second[0]; i++) {
+            for(var x = first[1]; x <= second[1]; x++) {
+                lights[i, x] += 2;
+            }
+        }
+    }
+    
+    private static long CountBrightness(int[,] lights) {
+        var count = 0;
+        for(var i = 0; i < 1000; i++) {
+            for(var x = 0; x < 1000; x++) {
+                count += lights[i, x];
+            }
+        }
+        return count;
     }
 }
